66df5197ce5f818dfb452c1f7b156ed376ca1bdd
Chou <jom6302@gmail.com>
2023-03-15 02:22:48 +0800

    first commit

---
+++ product.service/src/main/java/com/destinyscrew/product/service/controller/ProductController.java
@@ -0,0 +1,32 @@
+package com.destinyscrew.product.service.controller;
+
+import com.destinyscrew.product.service.dto.ProductRequest;
+import com.destinyscrew.product.service.dto.ProductResponse;
+import com.destinyscrew.product.service.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    @Autowired
+    private final ProductService productService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createProduct(@RequestBody ProductRequest productRequest){
+        productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts(){
+        return productService.getAllProducts();
+    }
+}
